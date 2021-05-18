e2a5638c4da795c6fcc1bea87720ecb34f72df76
Matus <matus@bitfluxventures.com>
2021-05-18 17:57:04 +0200

    Project init

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Boards from '../views/Boards.vue'
+import BoardDetail from '../views/BoardDetail.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Boards',
+    component: Boards
+  },
+  {
+    path: '/board/:id',
+    name: 'BoardDetail',
+    component: BoardDetail
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
