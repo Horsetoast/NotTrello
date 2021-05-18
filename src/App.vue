127534202e0272c7a1075c4bdbff27c7ddd0d137
Matus <matus@bitfluxventures.com>
2021-05-18 18:13:36 +0200

    feat: added NavigationHeader component with some basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
+    <NavigationHeader/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavigationHeader from '@/components/NavigationHeader'
+export default {
+  components: {
+    NavigationHeader
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 }
 </style>
---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="header">
+    <h1>NotTrello™</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+#header {
+  height: 60px;
+  background: var(--white);
+  color: var(--gray-300);
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  box-shadow: var(--shadow-sm);
+  h1 {
+    font-weight: bold;
+    font-size: var(--text-md);
+  }
+}
+</style>
