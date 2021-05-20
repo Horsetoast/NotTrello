7e80bd2d9d3e8c51c4d88e506a5e6338f265b95d
Matus <matus@bitfluxventures.com>
2021-05-20 13:48:28 +0200

    refactor: styles folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/assets/main.scss'
+import '@/assets/styles/main.scss'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
