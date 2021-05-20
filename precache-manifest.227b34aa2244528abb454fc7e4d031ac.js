50c26127c65e87fdebfc2c1d1b557f212db1b4fc
Matus <matus@bitfluxventures.com>
2021-05-20 14:55:37 +0200

    deploy

---
+++ precache-manifest.227b34aa2244528abb454fc7e4d031ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaafe1b2de8dbbe5f36",
+    "url": "/NotTrello/css/app.c266404a.css"
+  },
+  {
+    "revision": "d914611b28abf7d13c908100009c6ac4",
+    "url": "/NotTrello/index.html"
+  },
+  {
+    "revision": "4aaafe1b2de8dbbe5f36",
+    "url": "/NotTrello/js/app.7574c71a.js"
+  },
+  {
+    "revision": "36ddcc1f432f5a34a348",
+    "url": "/NotTrello/js/chunk-vendors.18e84d8b.js"
+  },
+  {
+    "revision": "78d2bec2faf0f9069264ebfd501ef2aa",
+    "url": "/NotTrello/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NotTrello/robots.txt"
+  }
+]);
